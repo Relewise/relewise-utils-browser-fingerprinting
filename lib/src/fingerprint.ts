662044af71e7d4b424813cb5a98f8fb468f89691
Martin Zanoni <mza@relewise.com>
2023-01-11 10:32:18 +0100

    remove utf-8 decoding

--- lib/src/fingerprint.ts
+++ lib/src/fingerprint.ts
@@ -6,13 +6,7 @@ export class Fingerprint {
      * @param salt 
      * @returns Fingerprint identifier
      */
-    static async get(salt: string, format?: 'base64'| 'uft-8'): Promise<string | null> {
-        return format === 'uft-8'
-            ? Fingerprint.getUft8(salt)
-            : Fingerprint.getBase64(salt);
-    }
-
-    static async getBase64(salt: string): Promise<string | null> {
+    static async get(salt: string): Promise<string | null> {
         try {
             const id = `${this.browserFingerprint()}_${salt}_${this.getRotator()}`;
 
@@ -23,26 +17,6 @@ export class Fingerprint {
             const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join('');
 
             return hashHex.substring(0, 32);
-        } catch {
-            if (crypto.subtle === undefined) {
-                console.error('window.crypto is null. In general, you should use this API only in secure contexts (HTTPS).')
-            } else {
-                console.error('Failed to generate fingerprint.');
-            }
-
-            return null;
-        }
-    }
-
-    static async getUft8(salt: string): Promise<string | null> {
-        try {
-            const id = `${this.browserFingerprint()}_${salt}_${this.getRotator()}`;
-
-            const encoder = new TextEncoder();
-            const hashedValue: ArrayBuffer = await crypto.subtle.digest('SHA-256', encoder.encode(id));
-
-            const decoder = new TextDecoder()
-            return decoder.decode(hashedValue)
         } catch {
             if (crypto.subtle === undefined) {
                 console.error('window.crypto is null. In general, you should use this API only in secure contexts (HTTPS).')
